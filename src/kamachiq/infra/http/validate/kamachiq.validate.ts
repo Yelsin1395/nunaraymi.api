0ef6a0655366a8c0d2525c0d0fd3a6bc16eeec0e
Jesús Yelsin Broly <yelgari13@gmail.com>
2023-09-23 13:54:59 -0500

    rollback logger.

--- src/kamachiq/infra/http/validate/kamachiq.validate.ts
+++ src/kamachiq/infra/http/validate/kamachiq.validate.ts
@@ -2,13 +2,11 @@ import Joi, { ObjectSchema } from 'joi';
 import { ubigeoSchemaValidate } from '@shared/infra/http/validate/common/ubigeo.schema';
 import { currencySchemaValidate } from '@shared/infra/http/validate/common/currency.schema';
 import { INVOICE } from '@shared/common/constants';
-import { logger } from '@shared/infra/logger';
 import AppError from '@shared/infra/shared.exception';
 import regex from '@shared/common/regex';
 import { IKamachiqCreate } from '@kamachiq/core/interface/kamachiq.interface';
 
 export function createKamachiqValidateInput(input: IKamachiqCreate) {
-  logger.info(`Input: ${input}`);
   const schema: ObjectSchema<IKamachiqCreate> = Joi.object({
     ruc: Joi.string().regex(regex.ruc).required(),
     name: Joi.string().required(),
--- src/shared/common/response.ts
+++ src/shared/common/response.ts
@@ -1,12 +1,9 @@
 import { Response } from 'express';
-import { logger } from '@shared/infra/logger';
 
 export function responseOk(res: Response, data: any) {
-  logger.info(`Output: ${data}`);
   res.status(200).send({ status: 200, sucess: true, payload: data });
 }
 
 export function responseCreate(res: Response, id: any) {
-  logger.info(`Output: ${id}`);
   res.status(201).send({ status: 201, sucess: true, payload: { newId: id } });
 }
